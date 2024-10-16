53c94a4cfde5d3b6bb6bc5e890187953f0f5a3d4
derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-10-16 13:44:34 +0300

    added the form

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,19 @@ export default function App () {
   return (
     <div>
       <h1>Hello world</h1>
-      <form action=""></form>
+      <form action="">
+        <input 
+          type="text" 
+          name="item"
+           id="" 
+           placeholder="Enter the item name"/>
+        <input 
+          type="number" 
+          name="price" 
+          id="price"
+          placeholder="Enter the price" />
+        <input type="submit" value="add" />
+      </form>
     </div>
   )
 }
